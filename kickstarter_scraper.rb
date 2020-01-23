b8533e8ddeeefdc5e45b9b1a5ccaf3b8e2b92fdc
Robert M Ricci <rmricci121@gmail.com>
2020-01-23 15:02:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,9 +10,9 @@ require 'pry'
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
+  # html = File.read('fixtures/kickstarter.html')
   
-  kickstarter  = Nokogiri::HTML(html)
+  # kickstarter  = Nokogiri::HTML(html)
   
   projects = {}
   
