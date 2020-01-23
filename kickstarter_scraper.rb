ae4fa0cd054fc574c591ed6c089bdd71a41b15c7
Robert M Ricci <rmricci121@gmail.com>
2020-01-23 15:02:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,14 +7,19 @@ require 'pry'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 #project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter  = Nokogiri::HTML(html)
   
-binding.pry 
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+projects
 end
-
 
 create_project_hash
